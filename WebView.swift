f988a3b2958ca307538e89bbf66a0f8b19a4a55c
Studio228 <46874472+Studio228@users.noreply.github.com>
2021-02-20 01:30:13 -0500

    Create WebView.swift

---
+++ WebView.swift
@@ -0,0 +1,27 @@
+//
+//  WebView.swift
+//  WebView Test View
+//
+//  Created by Loden Stamper on 2/20/21.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView:UIViewRepresentable {
+    
+    let urlString:String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
